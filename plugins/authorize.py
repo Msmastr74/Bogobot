c00b683867f01f62191ab495d151a49718a3d20c
Msmastr74 <zontaniguitars@gmail.com>
2026-04-25 02:49:48 -0500

    Updating for new system

--- plugins/authorize.py
+++ plugins/authorize.py
@@ -7,14 +7,14 @@ async def setup(bot):
         if user.id not in bot.config["authorized_users"]:
             bot.config["authorized_users"].append(user.id)
             bot.save_config()
-            return await interaction.response.send_message(f"<:Sorted:1495837069996720249> User {user.display_name} has been authorized", ephemeral=True)
+            return await bot.discord.messages.send(f"<:Sorted:1495837069996720249> User {user.display_name} has been authorized", response=True, ephemeral=True)
         else:
-            return await interaction.response.send_message(f"<:Unsorted:1495837051235598346> User {user.display_name} is already authorized", ephemeral=True)
+            return await bot.discord.messages.send(f"<:Unsorted:1495837051235598346> User {user.display_name} is already authorized", response=True, ephemeral=True)
     @bot.setup.command(name="deauthorize", description="Revokes a user's authorization", perm_requirement=2)
     async def deauthorize(interaction: discord.Interaction, user: discord.Member):
         if user.id in bot.config["authorized_users"]:
             bot.config["authorized_users"].remove(user.id)
             bot.save_config()
-            return await interaction.response.send_message(f"<:Sorted:1495837069996720249> User {user.display_name} has been deauthorized", ephemeral=True)
+            return await bot.discord.messages.send(f"<:Sorted:1495837069996720249> User {user.display_name} has been deauthorized", response=True, ephemeral=True)
         else:
-            return await interaction.response.send_message(f"<:Unsorted:1495837051235598346> User {user.display_name} isnt authorized",ephemeral=True)
+            return await bot.discord.messages.send(f"<:Unsorted:1495837051235598346> User {user.display_name} isnt authorized", response=True, ephemeral=True)
