b060f4c3de49fbb8274d2491558ccec4a0859a4a
Msmastr74 <zontaniguitars@gmail.com>
2026-04-25 04:58:31 -0500

    Fixing another bug
    Forgot to use the dictionary from info.get_stats_all lol

--- plugins/get_all_stats.py
+++ plugins/get_all_stats.py
@@ -6,10 +6,12 @@ async def setup(bot):
     @bot.setup.command(name="get_stats", description="Retrieve all current stream statistics", perm_requirement=0)
     async def get_all(interaction: discord.Interaction):
         
-        shuffles = await bot.info.get_shuffles()
-        comparisons = await bot.info.get_comparisons()
-        best_run = await bot.info.get_best_run()
-        shuffles_min = await bot.info.get_shuffles_min()
+        stats_list = await bot.info.get_stats_all()
+        
+        shuffles = stats_list["shuffles"]
+        comparisons = stats_list["comparisons"]
+        best_run = stats_list["best_run"]
+        shuffles_min = stats_list["shuffles_min"]
         serial = await bot.info.get_best_shuffle()
 
         bot.discord.embeds.send(title="Current Bogosort Statistics", color=discord.Color.green(), footer=f"Fetched at: {datetime.now().strftime('%H:%M:%S')}", response=True)
