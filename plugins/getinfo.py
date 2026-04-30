a9e9bec85d3917eb678796895fc5a7a6d56e9596
kan <kanyang808@gmail.com>
2026-04-30 20:13:15 +0800

    add avatar command

---
+++ plugins/getinfo.py
@@ -0,0 +1,10 @@
+import discord
+async def info_commands(bot):
+    @bot.setup.command(name="avatar", description="Get the avatar of a user", eph=False, perm_requirement=0)
+    async def avatar(inter: discord.Interaction, user: discord.User = None):
+        if user is None:
+            user = inter.user
+        embed = discord.Embed(title=f"{user.display_name}'s Avatar:")
+        embed.set_image(url=user.avatar.url)
+
+        await inter.response.send_message(embed=embed)
