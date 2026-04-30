c67bd953f3f524f4b4e5f649ba43daf5787bc1fc
kan <kanyang808@gmail.com>
2026-04-30 19:01:23 +0800

    randint

--- plugins/roll.py
+++ plugins/roll.py
@@ -8,3 +8,9 @@ async def setup(bot):
     @bot.setup.command(name="roll", description="Rolls a number from 1-100", eph=False, perm_requirement=0)
     async def roll(interaction: discord.Interaction):
         await bot.discord.messages.send(contents=f"{random.randint(1, 100)}", response=True)
+    @bot.setup.command(name="randint", description="Rolls a random number from user specified range", eph=False, perm_requirement=0)
+    async def randint(interaction: discord.Interaction, max: int, min: int = 1):
+        if max < min:
+            await bot.discord.messages.send(contents=f"Max must be bigger than min. Did you mean `/randint {min} {max}`?", response=True)
+            return
+        await bot.discord.messages.send(contents=f"{random.randint(min, max)}", response=True)
