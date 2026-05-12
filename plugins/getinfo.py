dbb11fc1c84d7e80bf2f8945ecf4af75df8e1437
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2026-05-12 14:59:15 -0400

    Add /ping

--- plugins/getinfo.py
+++ plugins/getinfo.py
@@ -4,6 +4,8 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from main import BotCore
 
+import time
+
 async def setup(bot: 'BotCore'):
     @bot.setup.command(name="avatar", description="Get the avatar of a user", eph=False, perm_requirement=0)
     async def avatar(interaction: discord.Interaction, user: discord.Member | discord.User | None = None) -> None:
@@ -12,4 +14,19 @@ async def setup(bot: 'BotCore'):
         embed = discord.Embed(title=f"{user.display_name}'s Avatar:")
         embed.set_image(url=user.display_avatar.url)
 
-        await interaction.followup.send(embed=embed)
+        await bot.discord.send_embed(embed=embed)
+    
+    @bot.setup.command(name="ping", description="Ping pong", defer=False, perm_requirement=0)
+    async def ping(interaction: discord.Interaction):
+        now = discord.utils.utcnow() 
+        ping_ms = (now - interaction.created_at).total_seconds() * 1000
+        if ping_ms > 500:
+            color = discord.Colour.red()
+        elif ping_ms > 200:
+            color = discord.Colour.orange()
+        else:
+            color = discord.Colour.blue()
+        embed = discord.Embed(title="Pong!", color=color, timestamp=now)
+        embed.add_field(name="Interaction Latency", value=f"{ping_ms:.2f} ms")
+        embed.add_field(name="Gateway Latency", value=f"{bot.latency * 1000:.2f} ms")
+        await bot.discord.send_embed(embed=embed)
