18d0658117b096d496bea373cbe3546fdd821204
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2026-05-14 21:56:57 -0400

    Remove the original elapsed_time from STATS_COORDS? Since when was that there?

--- bogobot_core.py
+++ bogobot_core.py
@@ -95,7 +95,6 @@ class BotCore(discord.Client):
             "comparisons": (331, 610, 551, 640),
             "best_run": (645, 610, 730, 640, "0123456789/.dhm "),
             "shuffles_sec": (819, 610, 1043, 640),
-            "elapsed_time": (1166, 0, 1180, 75),
             "average_best_shuffle": (80, 670, 115, 685, "0123456789/.dhm "),
             "uptime": (1160, 10, 1260, 30, "0123456789/.dhm ")
         }
