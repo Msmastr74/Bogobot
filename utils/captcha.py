01e84a37bcf714dc2722c485be7dcd62395beeca
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2026-06-20 15:20:52 -0400

    Remove 'E' and 'F' from captcha labels

--- utils/captcha.py
+++ utils/captcha.py
@@ -11,7 +11,7 @@ from PIL import Image, ImageDraw, ImageFilter, ImageFont
 
 
 SECRET_KEY = secrets.token_bytes(32)
-LABELS = list("ABCDEF")
+LABELS = list("ABCD")
 NUM_DISTRACTORS = len(LABELS) - 1
 MIN_START_DISTANCE = 60
 
